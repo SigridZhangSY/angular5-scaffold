d1bd84cfed03ba1677a408a0457dd6f2cd969783
syzhang <sigridzhang0603@163.com>
2018-06-14 01:09:50 +0800

    add router
    add prepush git hook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,46 +5,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BookComponent } from './book/book.component';
-import { BookDetailComponent } from './book-detail/book-detail.component';
-import { BookCreateComponent } from './book-create/book-create.component';
-import { BookEditComponent } from './book-edit/book-edit.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
 const appRoutes: Routes = [
   {
-    path: 'books',
-    component: BookComponent,
-    data: { title: 'Book List' }
+    path: 'first-page',
+    component: FirstPageComponent
   },
   {
-    path: 'book-details/:id',
-    component: BookDetailComponent,
-    data: { title: 'Book Details' }
-  },
-  {
-    path: 'book-create',
-    component: BookCreateComponent,
-    data: { title: 'Create Book' }
-  },
-  {
-    path: 'book-edit/:id',
-    component: BookEditComponent,
-    data: { title: 'Edit Book' }
+    path: 'second-page',
+    component: SecondPageComponent
   },
   { path: '',
-    redirectTo: '/books',
+    redirectTo: '/first-page',
     pathMatch: 'full'
   }
 ];
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent,
-    BookDetailComponent,
-    BookCreateComponent,
-    BookEditComponent,
-    HeaderComponent
+    HeaderComponent,
+    FirstPageComponent,
+    SecondPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/second-page/second-page.component.spec.ts
@@ -0,0 +1,7 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SecondPageComponent } from './second-page.component';
+
+describe('SecondPageComponent', () => {
+
+});
